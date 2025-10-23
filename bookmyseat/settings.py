d3efab447b320c8f7e901d1d0380b7c37a1afaef
prathamd24 <prathamkumard24@gmail.com>
2025-10-24 01:18:35 +0530

    Fix ALLOWED_HOSTS for Vercel subdomains

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -57,14 +57,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
-
 ]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 AUTH_USER_MODEL='auth.User'
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
