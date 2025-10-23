13abce917c533911c772dff85ef30dc16069ae02
prathamd24 <prathamkumard24@gmail.com>
2025-10-24 01:18:44 +0530

    Fix ALLOWED_HOSTS for Vercel subdomains

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
 
 ]
 
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL='auth.User'
@@ -137,8 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
