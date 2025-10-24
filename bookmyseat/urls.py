cb1282f272718ba444819f76bd5a672dd52e5b26
prathamd24 <prathamkumard24@gmail.com>
2025-10-24 17:18:17 +0530

    Serve media files on Render

--- bookmyseat/urls.py
+++ bookmyseat/urls.py
@@ -5,9 +5,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('',include('users.urls')),
+    path('', include('users.urls')),
     path('movies/', include('movies.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
